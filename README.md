511f65f2e556631bb79e69b5dced23107da914ce
Uier <ap9940506gmail.com>
2020-02-08 00:44:54 +0800

    complete profile UI & add Todo list

--- README.md
+++ README.md
@@ -1,24 +1,44 @@
 # pyshare-fe
 
+## Todo
+
+- [x] problems
+  - [x] UI
+  - [ ] API
+- [ ] problem
+  - [ ] UI
+  - [ ] API
+- [x] profile
+  - [x] UI
+  - [ ] API
+- [ ] admin
+  - [ ] UI
+  - [ ] API
+
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Run your unit tests
+
 ```
 npm run test:unit
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/views/profile.vue
+++ src/views/profile.vue
@@ -1,4 +1,79 @@
 <template>
+  <v-container
+    :style="{ width: $vuetify.breakpoint.mdAndUp ? '60vw' : '95vw', height: '100%' }"
+  >
+    <v-card height="100%" elevation="12">
+      <v-card-title>個人頁面</v-card-title>
+      <v-divider class="mt-0"></v-divider>
+      <v-card-text class="text--primary">
+        <v-row>
+          <v-col cols="auto" md="4">
+            <v-icon>mdi-account</v-icon>使用者名稱：
+          </v-col>
+          <v-col cols="auto" md="8">Username</v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="auto" md="4">
+            <v-icon>mdi-account-badge</v-icon>暱稱（網頁顯示名稱）：
+          </v-col>
+          <v-col cols="auto" md="8">displayName</v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="auto" md="4">
+            <v-icon>mdi-heart</v-icon>獲得愛心總數：
+          </v-col>
+          <v-col cols="auto" md="8">LIKES</v-col>
+        </v-row>
+      </v-card-text>
+
+      <v-form v-model="form" ref="form">
+        <v-card-title>更改密碼</v-card-title>
+        <v-divider class="mt-0"></v-divider>
+        <v-card-text>
+          <v-alert
+            v-model="errAlert"
+            dense
+            dismissible
+            :type="errType"
+            transition="slide-y-transition"
+          >{{ errMsg }}</v-alert>
+          <v-row>
+            <v-text-field
+              v-model="passwd.new"
+              label="請輸入新密碼"
+              class="mx-1"
+              type="password"
+              :rules="[v => !!v || '請輸入欲設置的新密碼！']"
+              outlined
+            ></v-text-field>
+            <v-text-field
+              label="請再次輸入新密碼"
+              class="mx-1"
+              type="password"
+              :rules="[v => !!v && v===passwd.new || '確認密碼與新密碼不相符。']"
+              outlined
+            ></v-text-field>
+            <v-text-field
+              v-model="passwd.old"
+              label="請輸入原密碼"
+              type="password"
+              :rules="[v => !!v || '請輸入原密碼！']"
+              class="mx-1"
+              outlined
+            ></v-text-field>
+          </v-row>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn 
+            block
+            dark
+            color="#4B8BBE"
+            @click="submit"
+          ><v-icon>mdi-send</v-icon>送出</v-btn>
+        </v-card-actions>
+      </v-form>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
@@ -8,7 +83,34 @@ export default {
 
   data () {
     return {
+      form: true,
+      errType: undefined,
+      errAlert: false,
+      errMsg: '',
+      passwd: {
+        new: '',
+        old: '',
+      },
+    }
+  },
 
+  methods: {
+    submit() {
+      if ( this.$refs.form.validate() ) {
+        // this.$http.post(`${API_BASE_URL}/auth/change-password`, this.passwd)
+          // .then((res) => {
+            // this.errMsg = '密碼更改成功';
+            // this.errType = 'success';
+            // this.errAlert = true;
+            // console.log(res);
+          // })
+          // .catch((err) => {
+            this.errMsg = '密碼更改失敗，原密碼錯誤。';
+            this.errType = 'error';
+            this.errAlert = true;
+            // console.log(err);
+          // })
+      }
     }
   }
 }

8962b771ea1c396deaff0f529acff562e4f920d1
skps2010 <skps2010030908@yahoo.com>
2020-03-27 09:50:24 +0800

    add GET /problem api

--- src/views/problems/index.js
+++ src/views/problems/index.js
@@ -2,48 +2,44 @@ import Vue from 'vue';
 import html from './index.pug';
 
 export default Vue.extend({
-  template: html,
-  
-  data () {
-    return {
-      items: [
-        { 
-          title: 'DSCP修課背景分析',
-          id: '1',
-          tags: ['分類一'],
-          timestamp: '',
-          author: {
-            username: 'tcc',
-            displayName: '蔣宗哲',
-          },
-        },
-        { 
-          title: '108-1 自行車竊案統計-3 (圖表繪製)',
-          id: '2',
-          tags: ['分類一', '分類二'],
-          timestamp: '',
-          author: {
-            username: 'tcc',
-            displayName: '蔣宗哲',
-          },
-        },
-        { 
-          title: '108-1 自行車竊案統計-2 (圖表繪製)',
-          id: '3',
-          tags: ['分類一', '分類二'],
-          timestamp: '',
-          author: {
-            username: 'tcc',
-            displayName: '蔣宗哲',
-          },
-        },
-      ]
-    }
-  },
+    template: html,
 
-  ready () {
-  },
+    data() {
+        return {
+            items: []
+        }
+    },
 
-  methods: {
-  },
+    ready() {},
+
+    beforeMount() {
+        this.getProblems()
+    },
+
+    methods: {
+        async getProblems() {
+            let result;
+            try {
+                result = await this.$http.get('/api/problem');
+            } catch (e) {
+                console.log(e);
+                result = {
+                    'data': [{
+                        pid: '1',
+                        title: 'DSCP修課背景分析',
+                        timestamp: '',
+                        author: {
+                            username: 'tcc',
+                            displayedName: '蔣宗哲',
+                        },
+                        status: 0
+                    }]
+                }
+            }
+            this.items = result.data.map((problem) => {
+                problem.tags = []
+                return problem
+            });
+        }
+    },
 });

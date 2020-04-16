17973162ceaa91586dfc8d9fe0378a9b93e9759b
Uier <ap9940506@gmail.com>
2020-04-17 03:09:13 +0800

    init tags manage (add js)

---
+++ src/views/admin/tag/index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue';
+import html from './index.pug';
+
+export default Vue.extend({
+  template: html,
+  
+  data () {
+    return {
+      
+    }
+  },
+
+  ready () {
+  },
+
+  methods: {
+  },
+});

809f685ca747dd52347be3a1489c2a4b25e4370f
Uier <ap9940506@gmail.com>
2020-04-17 11:00:29 +0800

    build tag manager ui

--- src/views/admin/tag/index.js
+++ src/views/admin/tag/index.js
@@ -6,7 +6,8 @@ export default Vue.extend({
   
   data () {
     return {
-      
+      items: ['分類一', '分類三'],
+      settings: [],
     }
   },
 
@@ -14,5 +15,26 @@ export default Vue.extend({
   },
 
   methods: {
+    myToggle() {
+      this.$nextTick(() => {
+        if ( this.selectAll() ) {
+          this.settings = [];
+        } else {
+          this.settings = [];
+          for ( let i=0; i<this.items.length; i++ ) this.settings.push(i);
+        }
+      })
+    },
+    selectAll() {
+      return this.settings.length === this.items.length
+    },
+    selectSome() {
+      return this.settings.length > 0 && !this.selectAll()
+    },
+    icon() {
+      if ( this.selectAll() ) return 'mdi-close-box'
+      if ( this.selectSome() ) return 'mdi-minus-box'
+      return 'mdi-checkbox-blank-outline'
+    },
   },
 });

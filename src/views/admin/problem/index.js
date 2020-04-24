329a9c76cf7fc6d7a7765cd7fc2688aea4e693bd
skps2010 <skps2010030908@yahoo.com>
2020-04-24 16:07:06 +0800

    finish admin/problems

--- src/views/admin/problem/index.js
+++ src/views/admin/problem/index.js
@@ -1,81 +1,116 @@
 import Vue from 'vue';
 import html from './index.pug';
+const UNLIMIT = '不限課程'
 
 export default Vue.extend({
-  template: html,
-  
-  data () {
-    return {
-      query: {
-        tag: '',
-        keyword: '',
-      },
-      /*
-      4 variables need api
+    template: html,
 
-      problems: []   2.2.1 get problem list
-        每個元素都是一個字典，內容物如下方的 items
+    data() {
+        return {
+            /*
+            4 variables need api
 
-      courses: []    2.4.1 get course  list
-        注意需有一個元素為"不限課程"
+            problems: []   2.2.1 get problem list
+              每個元素都是一個字典，內容物如下方的 items
 
-      tags: []       2.5.1 get Tag     list
-        注意 tags 有分課程，course變數 是哪堂課我就要哪堂課程的 tags
+            courses: []    2.4.1 get course  list
+              注意需有一個元素為"不限課程"
 
-      course: String 代表現在選擇課程的 value
+            tags: []       2.5.1 get Tag     list
+              注意 tags 有分課程，course變數 是哪堂課我就要哪堂課程的 tags
 
-      */
-      items: [
-        { 
-          title: 'DSCP修課背景分析',
-          id: '1',
-          tags: ['分類一'],
-          status: '顯示',
-          timestamp: '',
-          author: {
-            username: 'tcc',
-            displayName: '蔣宗哲',
-          },
+            course: String 代表現在選擇課程的 value
+
+            */
+            problems: [],
+            courses: [],
+            tags: [],
+            course: UNLIMIT,
+            selectedTags: [],
+            title: '',
+            status: ['顯示', '隱藏（老師和作者可見）']
+        }
+    },
+
+    ready() {},
+
+    beforeMount() {
+        this.getProblems()
+        this.getCourses()
+        this.getTags()
+    },
+
+    watch: {
+        course() {
+            this.getTags()
+        }
+    },
+
+    methods: {
+        async getProblems() {
+            let result;
+            try {
+                let filter = {
+                    offset: 0,
+                    count: -1,
+                    tags: this.selectedTags.join()
+                }
+                if (this.course != UNLIMIT) filter['course'] = this.course
+                if (this.title != '') filter['title'] = this.title
+
+                result = await this.$http.get('/api/problem', { params: filter });
+            } catch (e) {
+                console.log(e);
+                result = {
+                    'data': [{
+                        pid: '1',
+                        title: 'DSCP修課背景分析',
+                        timestamp: '',
+                        author: {
+                            username: 'tcc',
+                            displayedName: '蔣宗哲',
+                        },
+                        status: 0
+                    }]
+                }
+            }
+            this.problems = result.data.map(problem => {
+                problem.tags = []
+                return problem
+            });
         },
-        { 
-          title: '108-1 自行車竊案統計-3 (圖表繪製)',
-          id: '2',
-          tags: ['分類一', '分類二'],
-          status: '顯示',
-          timestamp: '',
-          author: {
-            username: 'tcc',
-            displayName: '蔣宗哲',
-          },
+
+        async getCourses() {
+            let result;
+            try {
+                result = await this.$http.get('/api/course');
+            } catch (e) {
+                console.log(e);
+                result = {
+                    'data': [{
+                        name: 'God',
+                        teacher: {
+                            username: 'tcc',
+                            displayedName: '蔣宗哲',
+                        }
+                    }]
+                }
+            }
+            this.courses = result.data.map(course => course.name).concat(UNLIMIT);
         },
-        { 
-          title: '108-1 自行車竊案統計-2 (圖表繪製)',
-          id: '3',
-          tags: ['分類一', '分類二'],
-          status: '顯示',
-          timestamp: '',
-          author: {
-            username: 'tcc',
-            displayName: '蔣宗哲',
-          },
-        },
-      ],
-      status: ['顯示', '隱藏（老師和作者可見）'],
-    }
-  },
 
-  ready () {
-  },
-
-  methods: {
-    /*
-  
-    problem filter 的 api 
-
-    query(course, tag, keyword) {
-      
-    }
-
-    */
-  },
+        async getTags() {
+            let result;
+            try {
+                result = await this.$http.get('/api/tag' + (this.course == UNLIMIT ? '' : '?course=' + this.course));
+            } catch (e) {
+                console.log(e);
+                result = {
+                    'data': ['tag1', 'tag2']
+                }
+            }
+            this.selectedTags = [];
+            this.tags = result.data;
+        }
+    },
 });

f09d901b395e4b3a80198ae053bc39dccffde197
Uier <ap9940506@gmail.com>
2020-04-25 17:53:04 +0800

    set up tag ui & ready to concat apis

--- src/views/admin/tag/index.js
+++ src/views/admin/tag/index.js
@@ -6,8 +6,28 @@ export default Vue.extend({
   
   data () {
     return {
-      items: ['分類一', '分類三'],
-      settings: [],
+      /*
+
+        api: 2.5.1 Get tag list
+        
+        selectedCourse: 選擇的課程
+        candidateTags: 目前可以加入到選擇課程的分類（剩餘沒選的）
+        availableTags: 目前該課程已經有的分類（之前已授權的可用分類）
+
+        selectedTags:  目前正在勾選的分類
+
+        newTagDialog:  新增分類的對話匡
+        
+
+        這部份我想也需要有一個元素為"不限課程"
+        不限課程的時候，availableTags 應為 empty, candidateTags 應為 server 上的所有 tag
+      */
+      candidateTags: ['分類一', '分類三'],
+      selectedCourse: null,
+      availableTags: ['分類二', '分類四', '分類五'],
+      selectedTags: [],
+      newTagDialog: false,
+      newTagNames: '',
     }
   },
 
@@ -15,26 +35,56 @@ export default Vue.extend({
   },
 
   methods: {
+
+    // 這兩個 api 都收 array! 詳看 api ref
+
+    // api: 2.5.2 Create tags
+    addNewTag(tags) {
+      console.log(tags)
+    },
+    // api: 2.5.3 Delete tags
+    deleteTags(tags) {
+      console.log(tags)
+    },
+    // api: 2.4.6 Manage tags
+    pushTags(course, tags) {
+      // course: string
+      // tags: array
+      console.log(course, tags)
+    },
+    popTag(course, tag) {
+      // course: string
+      // tag: string
+      console.log(course, tag)
+    },
+
+    /*
+      這裡應該不會動到
+      util to control SELECT ALL component
+    */
     myToggle() {
       this.$nextTick(() => {
         if ( this.selectAll() ) {
-          this.settings = [];
+          this.selectedTags = [];
         } else {
-          this.settings = [];
-          for ( let i=0; i<this.items.length; i++ ) this.settings.push(i);
+          this.selectedTags = [];
+          for ( let i=0; i<this.candidateTags.length; i++ ) this.selectedTags.push(i);
         }
       })
     },
     selectAll() {
-      return this.settings.length === this.items.length
+      return this.selectedTags.length === this.candidateTags.length
     },
     selectSome() {
-      return this.settings.length > 0 && !this.selectAll()
+      return this.selectedTags.length > 0 && !this.selectAll()
     },
     icon() {
       if ( this.selectAll() ) return 'mdi-close-box'
       if ( this.selectSome() ) return 'mdi-minus-box'
       return 'mdi-checkbox-blank-outline'
     },
+    iconColor() {
+      return (this.selectedTags.length > 0 ? 'indigo darken-4' : '')
+    }
   },
 });

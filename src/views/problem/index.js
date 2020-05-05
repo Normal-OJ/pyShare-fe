3ea73eec010cbf508453dc8b025af3f0a18d6744
Uier <ap9940506@gmail.com>
2020-05-05 18:06:36 +0800

    add menu

--- src/views/problem/index.js
+++ src/views/problem/index.js
@@ -24,6 +24,8 @@ export default Vue.extend({
                     text: '隱藏'
                 }
             },
+            menu: ['編輯', '刪除'],
+            isCommentEditing: [],  // Boolean
             isReplyShowed: [],  // Boolean
             replyInputs: [],    // {show: Boolean, text: String}
             /*
@@ -39,6 +41,10 @@ export default Vue.extend({
                 target: 'problem',
                 id: this.$route.params.id,
                 title: '',
+                content: '',
+                code: '',
+            },
+            editComment: {
                 content: '',
                 code: '',
             },
@@ -99,14 +105,42 @@ export default Vue.extend({
                                 timestamp: '2020/4/21',
                             }],
                           },
+                          {
+                            id: '123',
+                            author: {
+                                username: 'bogay',
+                                displayName: '莊博傑',
+                            },
+                            title: '留言標題',
+                            content: `切資料真的是很 重要的技術，要搞清楚T^T
+                                    切資料真的是很重要的技術，要搞清楚T^T
+                                    切資料真的是很重要的技術，要搞清楚T^T
+                                    `,
+                            code: `def a():`,
+                            result: `test`,
+                            timestamp: '2020/4/20',
+                            star: 7777,
+                            status: 1,
+                            reply: [{
+                                author: {
+                                    username: 'skps',
+                                    displayName: '盧昭華',
+                                },
+                                content: 'replying...',
+                                status: 1,
+                                timestamp: '2020/4/21',
+                            }],
+                          },
                         ],
                         defaultCode: 'push -f'
                     }
                 }
             }
             this.isReplyShowed = new Array(result.data.comments.length)
+            this.isCommentEditing = new Array(result.data.comments.length)
             this.replyInputs = new Array(result.data.comments.length)
             this.isReplyShowed.fill(false)
+            this.isCommentEditing.fill(false)
             this.replyInputs.fill({show: false, text: ''})
             this.problem = result.data
         },
@@ -121,7 +155,25 @@ export default Vue.extend({
                 })
             }
         },
-
+        menuTouch(opt, cls, idx, id) {
+            if ( opt == 'edit') {
+                if ( cls == 'comment' ) {
+                    this.editComment = {content: this.problem.comments[idx].content, code: this.problem.comments[idx].code}
+                    this.$set(this.isCommentEditing, idx, true)
+                } else {
+                    console.log('edit reply!');
+                }
+            } else {
+                this.delete(id);
+            }
+        },
+        cancelEditing(cls, idx) {
+            if ( cls == 'comment' ) {
+                this.$set(this.isCommentEditing, idx, false);
+            } else {
+                console.log('stop edit reply')
+            }
+        },
         /*
 
             留言是 對一個題目
@@ -132,17 +184,48 @@ export default Vue.extend({
             其他資訊看看 api ref 
 
         */ 
-        /*
-            這個檢查一下對不對 API 2.3.4
-        */
         likeComment(id) {
+            /*
+                這個檢查一下對不對 API 2.3.4
+            */
             this.$http.get(`/comment/${id}/like`)
         },
-        /*
-            新增一個留言 API 2.3.1
-        */
         addNewComment(data=this.newComment) {
+            /*
+                新增一個留言 API 2.3.1
+            */
+        },
+        addNewReply(_id, _content) {
+            data = {
+                target: 'comment',
+                id: _id,
+                title: '',
+                content: _content,
+                code: '',
+            }
+            /*
+                新增一個留言 API 2.3.1
+            */
+        },
+        update(id, data) {
+            /*
+                修改留言 API 2.3.2
+                id 是 欲修改的 comment 或 reply 的 id （route 要用的）
+                data 就直接放 api 的 parameter 就可以了
+            */
+        },
+        delete(cls, id) {
 
-        }
+        },
+        perm(user) {
+            /*
+                傳入一 username (string)
+                perm < 0 不是老師也不是作者（留言或回覆的作者）
+                perm = 0 是作者
+                perm = 1 是老師
+                （作者有修刪的權限、老師只有刪）
+            */
+            return 1
+        },
     },
 });

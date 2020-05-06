5a6d64faa062c896d62bbedb581679ec682ad758
skps2010 <skps2010030908@yahoo.com>
2020-05-06 13:36:38 +0800

    add course in watch()

--- src/views/admin/course/index.js
+++ src/views/admin/course/index.js
@@ -15,43 +15,7 @@ export default Vue.extend({
             newStudentFile: null,
             deleteStudentDialog: false,
             deleteStudentName: '',
-            studentData: [{
-                    username: 'studentA',
-                    displayName: 'stA',
-                    post: 2,
-                    comment: 2,
-                    reply: 2,
-                    starGive: 2,
-                    star: 2,
-                    success: 5,
-                    fail: 5,
-                    review: 0,
-                },
-                {
-                    username: 'studentB',
-                    displayName: 'stB',
-                    post: 1,
-                    comment: 1,
-                    reply: 0,
-                    starGive: 10,
-                    star: 500,
-                    success: 17,
-                    fail: 30,
-                    review: 1,
-                },
-                {
-                    username: 'studentC',
-                    displayName: 'stC',
-                    post: 0,
-                    comment: 5,
-                    reply: 15,
-                    starGive: 1,
-                    star: 5000,
-                    success: 1,
-                    fail: 2,
-                    review: 1,
-                },
-            ],
+            studentData: []
         }
     },
 
@@ -60,7 +24,55 @@ export default Vue.extend({
     },
 
     watch: {
-        course() {}
+        async course() {
+            let result;
+            try {
+                result = await this.$http.get(`/course/${this.course}/statistic`);
+            } catch (e) {
+                console.log(e);
+                result = {
+                    data: [{
+                            username: 'studentA',
+                            displayName: 'stA',
+                            post: 2,
+                            comment: 2,
+                            reply: 2,
+                            starGive: 2,
+                            star: 2,
+                            success: 5,
+                            fail: 5,
+                            review: 0,
+                        },
+                        {
+                            username: 'studentB',
+                            displayName: 'stB',
+                            post: 1,
+                            comment: 1,
+                            reply: 0,
+                            starGive: 10,
+                            star: 500,
+                            success: 17,
+                            fail: 30,
+                            review: 1,
+                        },
+                        {
+                            username: 'studentC',
+                            displayName: 'stC',
+                            post: 0,
+                            comment: 5,
+                            reply: 15,
+                            starGive: 1,
+                            star: 5000,
+                            success: 1,
+                            fail: 2,
+                            review: 1,
+                        },
+                    ],
+                }
+            }
+
+            this.studentData = result.data
+        }
     },
 
     methods: {
@@ -70,7 +82,7 @@ export default Vue.extend({
                 result = await this.$http.post('/course', {
                     name: name,
                     teacher: getProfile().username
-                }, { emulateJSON: true });
+                });
             } catch (e) {
                 console.log(e);
             }

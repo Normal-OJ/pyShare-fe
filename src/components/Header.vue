04965e64102edf306d6b0c9fc2264dd80633c9e1
TzuWei <40747024s@ntnu.edu.tw>
2020-10-03 16:03:13 +0800

    build home page & favicon & fonts

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app-bar app color="primary" height="56" elevation="0">
+    <v-img
+      class="mr-2"
+      :src="require('@/assets/images/logo.png')"
+      max-width="32"
+    />
+    <v-toolbar-title style="color: white;">
+      Python 創作分享平台
+    </v-toolbar-title>
+  </v-app-bar>
+</template>
+
+<script>
+export default {}
+</script>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -2,11 +2,22 @@ import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
 import zhHant from 'vuetify/es5/locale/zh-Hant'
 
+import colors from 'vuetify/lib/util/colors'
+
 Vue.use(Vuetify)
 
 export default new Vuetify({
   lang: {
     locales: { zhHant },
     current: 'zh-Hant',
+  },
+  theme: {
+    themes: {
+      light: {
+        primary: colors.lightBlue.darken3, // #0277BD, LightBlue 800
+        secondary: colors.pink.lighten2, // #F06292, Pink 300
+      },
+    },
+    options: { customProperties: true },
   },
 })

f18123a7908d80182ceeb3e51c6ae9b66313ea82
TzuWei <ap9940506@gmail.com>
2020-12-15 14:15:18 +0800

    add rollbar error tracking

--- src/main.js
+++ src/main.js
@@ -12,12 +12,30 @@ import '@mdi/font/css/materialdesignicons.css'
 import { apiSetup } from './api/api'
 import permission from './directive/permission'
 
+var Rollbar = require('vue-rollbar')
+const ROLLBAR_ENV_PRODUCTION = 'production'
+
 Vue.config.productionTip = false
 
 Vue.use(Fragment.Plugin)
 Vue.use(Vue2Editor)
 Vue.use(dayjsPlugin)
 Vue.use(utilsPlugin)
+Vue.use(Rollbar, {
+  accessToken: process.env.VUE_APP_ROLLBAR_TOKEN,
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+  enabled: true,
+  source_map_enabled: true,
+  environment: process.env.VUE_APP_ROLLBAR_ENV || ROLLBAR_ENV_PRODUCTION,
+  payload: {
+    client: {
+      javascript: {
+        code_version: '1.0',
+      },
+    },
+  },
+})
 Vue.directive('permission', permission)
 
 apiSetup()
